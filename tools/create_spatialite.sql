3665b46016745a26179df3ae406e49af19c62df4
Krist10an <krist10an@hotmail.com>
2015-03-21 12:29:49 +0100

    Register views to be able to load them directly in QGIS

--- tools/create_spatialite.sql
+++ tools/create_spatialite.sql
@@ -3,15 +3,27 @@ SELECT load_extension("libspatialite.so");
 
 SELECT InitSpatialMetadata();
 
+DROP VIEW spatial_trackslog;
 CREATE VIEW spatial_trackslog AS
-       SELECT t.*, MakePoint(lon, lat, 4326) as Geometry
+       SELECT t.rowid AS ROWID, t.*, MakePoint(lon, lat, 4326) as Geometry
          FROM trackslog t
           WHERE lat>0;
 
+INSERT INTO views_geometry_columns
+    (view_name, view_geometry, view_rowid, f_table_name, f_geometry_column)
+  VALUES ('spatial_trackslog', 'geometry', 'ROWID', 'trackslog', 'geometry');
+
+
+DROP VIEW spatial_flights;
 CREATE VIEW spatial_flights AS
-       SELECT t.modes, t.last_update, f.*, Makeline(t.Geometry) AS Geometry
+       SELECT t.rowid AS ROWID, t.modes, t.last_update, f.*, Makeline(t.Geometry) AS Geometry
          FROM spatial_trackslog t
           JOIN Flights f
             ON ( t.flightId = f.FlightID )
             GROUP BY t.flightId;
 
+INSERT INTO views_geometry_columns
+    (view_name, view_geometry, view_rowid, f_table_name, f_geometry_column)
+  VALUES ('spatial_flights', 'geometry', 'ROWID', 'trackslog', 'geometry');
+
+SELECT * FROM views_geometry_columns;
